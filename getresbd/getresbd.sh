1d521fa4cb614f31af174dce3c9ba9beab98257f
Валентин <123qwemmm@mail.ru>
2019-08-30 23:31:16 +0300

    getresbd

--- getresbd/getresbd.sh
+++ getresbd/getresbd.sh
@@ -3,10 +3,11 @@
 # $1 адрес ФТП id45d@id45d.myjino.ru:/domains/id45d.myjino.ru/BACKUPS/
 # $2 имя сервера: gazpromlog
 # $3 имя бэкапа без расширения: backup
-NAME_DIR_COPY=/home/$(whoami)/mongo_backup
-rm $NAME_DIR_COPY/$3.zip
-rm -rf $NAME_DIR_COPY/$3
-wget -O $NAME_DIR_COPY/$3.zip $1/$2/$3.zip
-unzip -d $NAME_DIR_COPY/$3.zip $NAME_DIR_COPY/
-rm $NAME_DIR_COPY/$3.zip
+DIR=/home/$(whoami)/mongo_backup
+rm $DIR/$3.zip
+rm -rf $DIR/$3
+wget -O $DIR/$3.zip $1/$2/$3.zip
+unzip -d $DIR/$3.zip $DIR/
+rm $DIR/$3.zip
+mongorestore --drop  --dir=$DIR/$3 --db $4 --numParallelCollections 1 --numInsertionWorkersPerCollection 1
 exit
--- restore_bd/restore_bd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# восстановление базы данных
-# $1 - имя базы данных
-# $2 - имя архива базы данных без .zip
-DIR_RESTORE=/home/$(whoami)/mongo_backup
-mongorestore --drop  --dir=$DIR_RESTORE/$2 --db $1 --numParallelCollections 1 --numInsertionWorkersPerCollection 1
-exit
